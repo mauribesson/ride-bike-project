de28ad5e39a4c5e0896f7e609a48d59d34d78bc1
Mauricio Bessón <mauribesson@gmail.com>
2023-06-18 13:49:53 -0300

    add: bootstrap y bootswatch

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def admin():
-    return render_template("index.jinja")
+    return render_template("index.html")
