14b3b0b349e80b579b034235d747f921a580ef2c
Mauricio Bessón <mauribesson@gmail.com>
2023-06-25 10:33:34 -0300

    add: Sobre nostros y ruteo

--- app/app.py
+++ app/app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def admin():
+def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
